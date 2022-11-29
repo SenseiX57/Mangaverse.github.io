6faea54fe35c77c28c87105364423a83e3f4c379
SenseiX57 <119361370+SenseiX57@users.noreply.github.com>
2022-11-30 03:08:55 +0530

    Add files via upload

---
+++ webd/login.php
@@ -0,0 +1,113 @@
+<?php
+//This script will handle login
+session_start();
+
+// check if the user is already logged in
+if(isset($_SESSION['username']))
+{
+    header("location: welcome.php");
+    exit;
+}
+require_once "config.php";
+
+$username = $password = "";
+$err = "";
+
+// if request method is post
+if ($_SERVER['REQUEST_METHOD'] == "POST"){
+    if(empty(trim($_POST['username'])) || empty(trim($_POST['password'])))
+    {
+        $err = "Please enter username + password";
+    }
+    else{
+        $username = trim($_POST['username']);
+        $password = trim($_POST['password']);
+    }
+
+
+if(empty($err))
+{
+    $sql = "SELECT id, username, password FROM users WHERE username = ?";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, "s", $param_username);
+    $param_username = $username;
+    
+    
+    // Try to execute this statement
+    if(mysqli_stmt_execute($stmt)){
+        mysqli_stmt_store_result($stmt);
+        if(mysqli_stmt_num_rows($stmt) == 1)
+                {
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt))
+                    {
+                        if(password_verify($password, $hashed_password))
+                        {
+                            // this means the password is corrct. Allow user to login
+                            session_start();
+                            $_SESSION["username"] = $username;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["loggedin"] = true;
+
+                            //Redirect user to welcome page
+                            header("location: welcome.php");
+                            
+                        }
+                    }
+
+                }
+
+    }
+}    
+
+
+}
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <link rel="stylesheet" href="loginstyle.css">
+    <title>MANGAVERSE Login Form</title>
+</head>
+<body>
+    
+    <div class="container">
+        <div class="forms">
+            <div class="form login">
+                <span class="title">Login</span>
+
+                <form action="" method="post">
+                    <div class="input-field">
+                        <input type="text" name="username" placeholder="Enter Username" required>
+                        <i class="uil uil-envelope icon"></i>
+                    </div>
+                    <div class="input-field">
+                        <input type="password" name="password" class="password" placeholder="Enter your password" required>
+                        <i class="uil uil-lock icon"></i>
+                    </div>
+ 
+                    <div class="checkbox-text">
+                        <div class="checkbox-content">
+                            <input type="checkbox" id="logCheck">
+                            <label for="logCheck" class="text">Remember me</label>
+                        </div>
+                    </div>
+
+                    <div class="input-field button">
+                        <input type="submit" class="btn btn-primary" value="Login">
+                    </div>
+                </form>
+
+            </div>
+
+        </div>
+    </div>
+  </body>
+</html>
---
+++ webd/shopcart/src/Data.js
@@ -0,0 +1,37 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Anya",
+    price: 449,
+    desc: "Here's anya merch from Spy X Family !",
+    img: "images/anya.webp",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Dabi T-Shirt",
+    price: 999,
+    desc: "You must have know Dabi from My Hero Academia !",
+    img: "images/dabi.webp",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Chainsaw T-Shirt",
+    price: 599,
+    desc: "Best Manga out there ! now chech this t-shirt ! ",
+    img: "images/chainsaw.webp",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "MOb Psycho T-Shirt",
+    price: 499,
+    desc: "Hey ! psychoooosss ! it's time to make other jealous!",
+    img: "images/mob.webp",
+  },
+  {
+    id: "thyfhcttt",
+    name: "Shank T-shirt",
+    price: 799,
+    desc: "I know he's your favourite side-character and yess! he's cool!",
+    img: "images/shank.webp",
+  },
+];
